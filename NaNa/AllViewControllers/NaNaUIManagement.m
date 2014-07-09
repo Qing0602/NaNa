401d71d78f84006699a7954609374c8343387b6e
wangshuo <9708022@qq.com>
2014-07-09 10:50:46 +0800

    network

---
+++ NaNa/AllViewControllers/NaNaUIManagement.m
@@ -0,0 +1,23 @@
+//
+//  PalmUIManagement.m
+//  teacher
+//
+//  Created by singlew on 14-3-7.
+//  Copyright (c) 2014年 ws. All rights reserved.
+//
+
+#import "NaNaUIManagement.h"
+
+@implementation NaNaUIManagement
+static NaNaUIManagement *sharedInstance = nil;
+
++(NaNaUIManagement *) sharedInstance{
+    @synchronized(sharedInstance){
+        if (nil == sharedInstance){
+            sharedInstance = [[NaNaUIManagement alloc] init];
+        }
+    }
+    return sharedInstance;
+}
+
+@end
