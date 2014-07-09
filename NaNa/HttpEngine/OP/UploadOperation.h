6ff2dfa6c0345fb3548e1da97f4d73063841f9a0
wangshuo <9708022@qq.com>
2014-07-09 19:54:18 +0800

    upload

---
+++ NaNa/HttpEngine/OP/UploadOperation.h
@@ -0,0 +1,13 @@
+//
+//  UploadOperation.h
+//  NaNa
+//
+//  Created by singlew on 14-7-9.
+//  Copyright (c) 2014年 dengfang. All rights reserved.
+//
+
+#import "NaNaOperation.h"
+
+@interface UploadOperation : NaNaOperation
+
+@end
---
+++ NaNa/HttpEngine/OP/UploadOperation.m
@@ -0,0 +1,13 @@
+//
+//  UploadOperation.m
+//  NaNa
+//
+//  Created by singlew on 14-7-9.
+//  Copyright (c) 2014年 dengfang. All rights reserved.
+//
+
+#import "UploadOperation.h"
+
+@implementation UploadOperation
+
+@end
