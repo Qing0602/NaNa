cf332ff95c9e0dc9a800203104da41fa03b84a3b
wangshuo <9708022@qq.com>
2014-08-08 04:04:30 +0800

    message operation

---
+++ NaNa/HttpEngine/OP/MessageOperation.h
@@ -0,0 +1,20 @@
+//
+//  MessageOperation.h
+//  NaNa
+//
+//  Created by singlew on 14-8-7.
+//  Copyright (c) 2014年 dengfang. All rights reserved.
+//
+
+#import "NaNaOperation.h"
+typedef enum{
+    kSendMessage,
+    kGetNewMessage,
+    kGetSideMessageList,
+}MessageType;
+
+@interface MessageOperation : NaNaOperation
+-(MessageOperation *) initSendMessage : (NSString *) content withUserID : (int) userID withTarGetID : (int) targetID;
+-(MessageOperation *) initGetNewMessage : (int) userID withTargetID : (int) targetID withPage : (int) page;
+-(MessageOperation *) initGetSideMessageList : (int) userID;
+@end
---
+++ NaNa/HttpEngine/OP/MessageOperation.m
@@ -0,0 +1,17 @@
+//
+//  MessageOperation.m
+//  NaNa
+//
+//  Created by singlew on 14-8-7.
+//  Copyright (c) 2014年 dengfang. All rights reserved.
+//
+
+#import "MessageOperation.h"
+
+@interface MessageOperation ()
+@property(nonatomic) MessageType type;
+@end
+
+@implementation MessageOperation
+
+@end
